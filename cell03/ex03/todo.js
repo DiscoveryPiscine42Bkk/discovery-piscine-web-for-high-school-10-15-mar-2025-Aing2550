b316b33690ddea45f1e04eb602f8c88769c7678b
Sutinan Sriviset <ssrivise@z2t11c3.42bangkok.com>
2025-03-13 12:56:13 +0700

    commit message

---
+++ cell03/ex03/todo.js
@@ -0,0 +1,52 @@
+
+        // Load existing tasks from cookies
+        document.addEventListener("DOMContentLoaded", () => {
+            let tasks = getTasksFromCookies();
+            tasks.forEach(task => addTaskToDOM(task));
+        });
+
+        document.getElementById("new-btn").addEventListener("click", () => {
+            let taskText = prompt("Enter a new task:");
+            if (taskText) {
+                addTaskToDOM(taskText);
+                saveTaskToCookies();
+            }
+        });
+
+        function addTaskToDOM(taskText) {
+            let taskDiv = document.createElement("div");
+            taskDiv.className = "todo-item";
+            taskDiv.textContent = taskText;
+            
+            // Add click event for deletion
+            taskDiv.addEventListener("click", () => {
+                let confirmDelete = confirm("Do you want to delete this task?");
+                if (confirmDelete) {
+                    taskDiv.remove();
+                    saveTaskToCookies();
+                }
+            });
+
+            // Insert at the top of the list
+            let taskList = document.getElementById("ft_list");
+            taskList.insertBefore(taskDiv, taskList.firstChild);
+        }
+
+        function saveTaskToCookies() {
+            let tasks = [];
+            document.querySelectorAll(".todo-item").forEach(task => {
+                tasks.push(task.textContent);
+            });
+            document.cookie = `tasks=${JSON.stringify(tasks)}; path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
+        }
+
+        function getTasksFromCookies() {
+            let cookies = document.cookie.split("; ");
+            for (let cookie of cookies) {
+                let [name, value] = cookie.split("=");
+                if (name === "tasks") {
+                    return JSON.parse(value);
+                }
+            }
+            return [];
+        }
